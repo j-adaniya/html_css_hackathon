b9ab87903de1a0d865731d85eef13373352a1d03
jun <j-adaniya@o-dream.jp>
2024-05-28 08:57:04 +0900

    initial commit

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,27 @@
 // ここにJavaScriptを記述していきます。
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("header a").forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = anchor.getAttribute("href");
+      const tragetElement = document.querySelector(id);
+      const top =
+        targetElement.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({ top: top, behavior: "smooth" });
+    });
+  });
+  const readMore = document.getElementById("read-more");
+  readMore.addEventListener("click", () => {
+    const moreText = document.getElementById("more-text");
+    if (readMore.classList.contains("active")) {
+      moreText.style.display = "none";
+      readMore.classList.remove("active");
+      readMore.textContent = "もっと詳しく";
+    } else {
+      moreText.style.display = "block";
+      readMore.classList.add("active");
+      readMore.textContent = "閉じる";
+    }
+  });
+  // 言語一覧部分
+});
